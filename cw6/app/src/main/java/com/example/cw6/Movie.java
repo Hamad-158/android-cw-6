bf098a2a957388f72d1869cec5f76ed0e60b4922
Hamad-158 <Hamad.bfar@gmail.com>
2020-07-01 17:52:30 +0300

    done

---
+++ cw6/app/src/main/java/com/example/cw6/Movie.java
@@ -0,0 +1,10 @@
+package com.example.cw6;
+
+public class Movie {
+
+    private  String title;
+    private String moinActor;
+    private Double movieRote;
+    private  int pgRate;
+    private String genre;
+}
